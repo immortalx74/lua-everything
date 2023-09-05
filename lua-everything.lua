a64459f1a3d079e7d6809da8ffd9de89318d026b
John Dodis <jfdodis@yahoo.com>
2023-09-05 10:21:40 +0300

    sort, match case and others...

--- lua-everything.lua
+++ lua-everything.lua
@@ -53,31 +53,63 @@ ffi.cdef [[
 	LPCTSTR Everything_GetResultPathA(int index);
 	LPCTSTR Everything_GetResultExtensionA(DWORD dwIndex);
 	void Everything_Reset(void);
+	void Everything_CleanUp(void);
+	void Everything_SetMatchCase(BOOL bEnable);
 	BOOL Everything_GetResultDateCreated(DWORD dwIndex, FILETIME *lpDateCreated);
 	BOOL Everything_GetResultDateModified(DWORD dwIndex, FILETIME *lpDateModified);
 	BOOL Everything_GetResultDateAccessed(DWORD dwIndex, FILETIME *lpDateAccessed);
 	DWORD Everything_GetLastError(void);
+	BOOL Everything_IsAdmin(void);
+	void Everything_SetMax(DWORD dwMaxResults);
+	void Everything_SetSort(DWORD dwSortType);
 
 	BOOL FileTimeToSystemTime(const FILETIME *lpFileTime, LPSYSTEMTIME lpSystemTime);
 ]]
 
-local m = {}
-m.REQUEST_FILE_NAME = 0x00000001
-m.REQUEST_PATH = 0x00000002
-m.REQUEST_FULL_PATH_AND_FILE_NAME = 0x00000004
-m.REQUEST_EXTENSION = 0x00000008
-m.REQUEST_SIZE = 0x00000010
-m.REQUEST_DATE_CREATED = 0x00000020
-m.REQUEST_DATE_MODIFIED = 0x00000040
-m.REQUEST_DATE_ACCESSED = 0x00000080
-m.REQUEST_ATTRIBUTES = 0x00000100
-m.REQUEST_FILE_LIST_FILE_NAME = 0x00000200
-m.REQUEST_RUN_COUNT = 0x00000400
-m.REQUEST_DATE_RUN = 0x00000800
-m.REQUEST_DATE_RECENTLY_CHANGED = 0x00001000
-m.REQUEST_HIGHLIGHTED_FILE_NAME = 0x00002000
-m.REQUEST_HIGHLIGHTED_PATH = 0x00004000
-m.REQUEST_HIGHLIGHTED_FULL_PATH_AND_FILE_NAME = 0x00008000
+local m                                            = {}
+m.REQUEST_FILE_NAME                                = 0x00000001
+m.REQUEST_PATH                                     = 0x00000002
+m.REQUEST_FULL_PATH_AND_FILE_NAME                  = 0x00000004
+m.REQUEST_EXTENSION                                = 0x00000008
+m.REQUEST_SIZE                                     = 0x00000010
+m.REQUEST_DATE_CREATED                             = 0x00000020
+m.REQUEST_DATE_MODIFIED                            = 0x00000040
+m.REQUEST_DATE_ACCESSED                            = 0x00000080
+m.REQUEST_ATTRIBUTES                               = 0x00000100
+m.REQUEST_FILE_LIST_FILE_NAME                      = 0x00000200
+m.REQUEST_RUN_COUNT                                = 0x00000400
+m.REQUEST_DATE_RUN                                 = 0x00000800
+m.REQUEST_DATE_RECENTLY_CHANGED                    = 0x00001000
+m.REQUEST_HIGHLIGHTED_FILE_NAME                    = 0x00002000
+m.REQUEST_HIGHLIGHTED_PATH                         = 0x00004000
+m.REQUEST_HIGHLIGHTED_FULL_PATH_AND_FILE_NAME      = 0x00008000
+
+m.SORT_NAME_ASCENDING                   = 1
+m.SORT_NAME_DESCENDING                  = 2
+m.SORT_PATH_ASCENDING                   = 3
+m.SORT_PATH_DESCENDING                  = 4
+m.SORT_SIZE_ASCENDING                   = 5
+m.SORT_SIZE_DESCENDING                  = 6
+m.SORT_EXTENSION_ASCENDING              = 7
+m.SORT_EXTENSION_DESCENDING             = 8
+m.SORT_TYPE_NAME_ASCENDING              = 9
+m.SORT_TYPE_NAME_DESCENDING             = 10
+m.SORT_DATE_CREATED_ASCENDING           = 11
+m.SORT_DATE_CREATED_DESCENDING          = 12
+m.SORT_DATE_MODIFIED_ASCENDING          = 13
+m.SORT_DATE_MODIFIED_DESCENDING         = 14
+m.SORT_ATTRIBUTES_ASCENDING             = 15
+m.SORT_ATTRIBUTES_DESCENDING            = 16
+m.SORT_FILE_LIST_FILENAME_ASCENDING     = 17
+m.SORT_FILE_LIST_FILENAME_DESCENDING    = 18
+m.SORT_RUN_COUNT_ASCENDING              = 19
+m.SORT_RUN_COUNT_DESCENDING             = 20
+m.SORT_DATE_RECENTLY_CHANGED_ASCENDING  = 21
+m.SORT_DATE_RECENTLY_CHANGED_DESCENDING = 22
+m.SORT_DATE_ACCESSED_ASCENDING          = 23
+m.SORT_DATE_ACCESSED_DESCENDING         = 24
+m.SORT_DATE_RUN_ASCENDING               = 25
+m.SORT_DATE_RUN_DESCENDING              = 26
 
 function m.GetNumResults()
 	return e.Everything_GetNumResults()
@@ -184,6 +216,32 @@ function m.GetResultDateAccessed( index )
 	local res = e.Everything_GetResultDateAccessed( index - 1, ft )
 	kernel32.FileTimeToSystemTime( ft, st )
 	return st[ 0 ].wYear, st[ 0 ].wMonth, st[ 0 ].wDayOfWeek, st[ 0 ].wDay, st[ 0 ].wHour, st[ 0 ].wMinute, st[ 0 ].wSecond, st[ 0 ].wMilliseconds
+end
+
+function m.CleanUp()
+	e.Everything_CleanUp();
+end
+
+function m.IsAdmin()
+	local is_admin = false
+	if e.Everything_IsAdmin() ~= 0 then is_admin = true end
+	return is_admin;
+end
+
+function m.SetMatchCase( enable )
+	if enable then
+		e.Everything_SetMatchCase( 1 )
+	else
+		e.Everything_SetMatchCase( 0 )
+	end
+end
+
+function m.SetMax( num_results )
+	e.Everything_SetMax( num_results )
+end
+
+function m.SetSort( sort )
+	e.Everything_SetSort( sort )
 end
 
 return m
--- main.lua
+++ main.lua
@@ -1,18 +1,25 @@
 local Everything = require( "lua-everything" )
 
 Everything.SetSearch( "Program Files" )
+Everything.SetMatchCase( true )
 Everything.SetRequestFlags( Everything.REQUEST_FILE_NAME, Everything.REQUEST_PATH, Everything.REQUEST_EXTENSION, Everything.REQUEST_SIZE, Everything.REQUEST_DATE_CREATED,
 	Everything.REQUEST_DATE_MODIFIED, Everything.REQUEST_DATE_ACCESSED )
-Everything.Query( true )
-local count = Everything.GetNumResults()
-print( "flags: " .. Everything.GetRequestFlags() )
 
--- for filename = 1, count do
--- 	print( Everything.GetResultFilename( filename ) )
--- end
+Everything.SetSort( Everything.SORT_ATTRIBUTES_DESCENDING )
+Everything.Query( true )
+print( "flags: " .. Everything.GetRequestFlags() )
+local count = Everything.GetNumResults()
+print( "num results: ", count )
+
+for filename = 1, count do
+	print( Everything.GetResultFilename( filename ) )
+end
 
 print( "size: " .. tostring( Everything.GetResultSize( 3 ) ) )
 print( Everything.IsFileResult( 1 ) )
 print( Everything.GetResultPath( 2 ) )
 print( Everything.GetResultExtension( (3) ) )
 print( Everything.GetResultDateAccessed( 4 ) )
+print( Everything.IsAdmin() )
+
+Everything.CleanUp()
